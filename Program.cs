ed5464d5ea0766fdb6cbba408e625af744d79858
anitabaigorria <abaigorriafacet@gmail.com>
2026-05-26 20:18:34 -0300

    Termine el Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -1,36 +1,191 @@
-﻿// PUNTO 0
-Console.WriteLine("Hello, World!"); 
-int a; 
-int b; 
-a=10; 
-b=a; 
-Console.WriteLine("valor de a:"+a); 
-Console.WriteLine("valor de b:"+b);
+﻿// // PUNTO 0
+// Console.WriteLine("Hello, World!"); 
+// int a; 
+// int b; 
+// a=10; 
+// b=a; 
+// Console.WriteLine("valor de a:"+a); 
+// Console.WriteLine("valor de b:"+b);
 
-// PROBLEMA 1
-    string textoBuscado = Console.ReadLine();
-    int numero;
+// // PROBLEMA 1
+//     string textoBuscado = Console.ReadLine();
+//     int numero;
+// do
+// {
+//     Console.Write("Ingrese un número mayor a 0: ");
+
+
+//     if (int.TryParse(textoBuscado, out numero))
+//     {
+//         if (numero > 0)
+//         {
+//             for (int i = textoBuscado.Length-1; i >= 0 ; i--)
+//             {
+//                 Console.Write($"{textoBuscado[i]}");            
+//             }   
+//         } else
+//         {
+//            Console.WriteLine("El numero ingresado es menor a 0. Intente nuevamente");    
+//         }
+//     } else
+//     {
+//         Console.WriteLine("Por favor, ingrese un numero. Intente nuevamente");
+//     }
+// } while (numero <= 0 );
+
+// PROBLEMA 2
+string eleccion,ingreso1,ingreso2;
+double num1,num2;
 do
 {
-    Console.Write("Ingrese un número mayor a 0: ");
+    Console.WriteLine("");
+    Console.WriteLine("-------- CALCULADORA --------");
+    Console.WriteLine("¿Qué operación desea realizar?");
+    Console.WriteLine("1 = Suma");
+    Console.WriteLine("2 = Resta");
+    Console.WriteLine("3 = Multiplicación");
+    Console.WriteLine("4 = Dividisión");
+    Console.WriteLine("0 = SALIR");
+    Console.WriteLine("------------------------------");
+    Console.WriteLine("");
+    
+    Console.Write("Ingrese: ");
+    eleccion = Console.ReadLine();
 
-
-    if (int.TryParse(textoBuscado, out numero))
+    
+    switch (eleccion)
     {
-        if (numero > 0)
-        {
-            for (int i = textoBuscado.Length-1; i >= 0 ; i--)
+        case "1":
+            Console.Write("Ingrese el primer numero a sumar (_ + ): ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
             {
-                Console.Write($"{textoBuscado[i]}");            
-            }   
-        } else
-        {
-           Console.WriteLine("El numero ingresado es menor a 0. Intente nuevamente");    
-        }
-    } else
-    {
-        Console.WriteLine("Por favor, ingrese un numero. Intente nuevamente");
-    }
-} while (numero <= 0 );
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese el primer numero a sumar (_ + ): ");
+                ingreso1 = Console.ReadLine();
+            }
 
+            Console.Write($"Ingrese el segundo numero a sumar ({num1} + _): ");
+            ingreso2 = Console.ReadLine();
+
+            while (double.TryParse(ingreso2, out num2) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write($"Ingrese el segundo numero a sumar ({num1} + _): ");
+                ingreso2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"    {num1}");
+            Console.WriteLine("+");
+            Console.WriteLine($"    {num2}");
+            Console.WriteLine("  __________");
+            Console.WriteLine($"    {num1 + num2}");
+            Console.WriteLine("");
+            
+            break;
+        case "2":
+            Console.Write("Ingrese el primer numero a restar (_ - ): ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese el primer numero a restar (_ - ): ");
+                ingreso1 = Console.ReadLine();
+            }
+
+            Console.Write($"Ingrese el segundo numero a restar ({num1} - _): ");
+            ingreso2 = Console.ReadLine();
+
+            while (double.TryParse(ingreso2, out num2) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write($"Ingrese el segundo numero a restar ({num1} - _): ");
+                ingreso2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"    {num1}");
+            Console.WriteLine("-");
+            Console.WriteLine($"    {num2}");
+            Console.WriteLine("  __________");
+            Console.WriteLine($"    {num1 - num2}");
+            Console.WriteLine("");
+            
+            break;        
+        case "3":
+            Console.Write("Ingrese el primer numero a multiplicar (_ * ): ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese el primer numero a multiplicar (_ * ): ");
+                ingreso1 = Console.ReadLine();
+            }
+
+            Console.Write($"Ingrese el segundo numero a multiplicar ({num1} * _): ");
+            ingreso2 = Console.ReadLine();
+
+            while (double.TryParse(ingreso2, out num2) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write($"Ingrese el segundo numero a multiplicar ({num1} * _): ");
+                ingreso2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"    {num1}");
+            Console.WriteLine("*");
+            Console.WriteLine($"    {num2}");
+            Console.WriteLine("  __________");
+            Console.WriteLine($"    {num1 * num2}");
+            Console.WriteLine("");
+            
+            break;        
+        case "4":
+            Console.Write("Ingrese el primer numero a dividir (_ / ): ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese el primer numero a dividir (_ / ): ");
+                ingreso1 = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ingrese el segundo numero a dividir ({num1} / _) ");
+            Console.WriteLine("IMPORTANTE: EL NUMERO DEBE SER MAYOR O IGUAL QUE 0");
+            ingreso2 = Console.ReadLine();
+
+            while (double.TryParse(ingreso2, out num2) == false || num2 <= 0)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.WriteLine($"Ingrese el segundo numero a dividir ({num1} / _) ");
+                Console.WriteLine("IMPORTANTE: EL NUMERO DEBE SER MAYOR O IGUAL QUE 0");
+                ingreso2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"    {num1}");
+            Console.WriteLine("/");
+            Console.WriteLine($"    {num2}");
+            Console.WriteLine("  __________");
+            Console.WriteLine($"    {num1 / num2}");
+            Console.WriteLine("");
+            
+            break;   
+        case "0":
+            Console.WriteLine("Saliendo de la calculadora...");
+            break;
+        
+        default:
+            // ingresa un valor no admitido
+            Console.WriteLine("Error: Opción no válida. Intente nuevamente"); 
+            break;
+    }
+
+} while (eleccion != "0"); // no convierto a numero la eleccion ya que solo comparo, lo trabajo como string
 // hacer menu de eleccion de enunciado
