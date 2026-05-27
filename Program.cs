62fab63bbfc9ca557e0a0f6d55e9130cbe3f7ee4
anitabaigorria <abaigorriafacet@gmail.com>
2026-05-27 00:42:37 -0300

    Termine el Ejercicio 3

--- Program.cs
+++ Program.cs
@@ -1,37 +1,37 @@
-﻿// // PUNTO 0
-// Console.WriteLine("Hello, World!"); 
-// int a; 
-// int b; 
-// a=10; 
-// b=a; 
-// Console.WriteLine("valor de a:"+a); 
-// Console.WriteLine("valor de b:"+b);
+﻿// PUNTO 0
+Console.WriteLine("Hello, World!"); 
+int a; 
+int b; 
+a=10; 
+b=a; 
+Console.WriteLine("valor de a:"+a); 
+Console.WriteLine("valor de b:"+b);
 
-// // PROBLEMA 1
-//     string textoBuscado = Console.ReadLine();
-//     int numero;
-// do
-// {
-//     Console.Write("Ingrese un número mayor a 0: ");
+// PROBLEMA 1
+    string textoBuscado = Console.ReadLine();
+    int numero;
+do
+{
+    Console.Write("Ingrese un número mayor a 0: ");
 
 
-//     if (int.TryParse(textoBuscado, out numero))
-//     {
-//         if (numero > 0)
-//         {
-//             for (int i = textoBuscado.Length-1; i >= 0 ; i--)
-//             {
-//                 Console.Write($"{textoBuscado[i]}");            
-//             }   
-//         } else
-//         {
-//            Console.WriteLine("El numero ingresado es menor a 0. Intente nuevamente");    
-//         }
-//     } else
-//     {
-//         Console.WriteLine("Por favor, ingrese un numero. Intente nuevamente");
-//     }
-// } while (numero <= 0 );
+    if (int.TryParse(textoBuscado, out numero))
+    {
+        if (numero > 0)
+        {
+            for (int i = textoBuscado.Length-1; i >= 0 ; i--)
+            {
+                Console.Write($"{textoBuscado[i]}");            
+            }   
+        } else
+        {
+           Console.WriteLine("El numero ingresado es menor a 0. Intente nuevamente");    
+        }
+    } else
+    {
+        Console.WriteLine("Por favor, ingrese un numero. Intente nuevamente");
+    }
+} while (numero <= 0 );
 
 // PROBLEMA 2
 string eleccion,ingreso1,ingreso2;
@@ -44,7 +44,13 @@ do
     Console.WriteLine("1 = Suma");
     Console.WriteLine("2 = Resta");
     Console.WriteLine("3 = Multiplicación");
-    Console.WriteLine("4 = Dividisión");
+    Console.WriteLine("4 = División");
+    Console.WriteLine("5 = Valor Absoluto de un numero");
+    Console.WriteLine("6 = Cuadrado de un numero");
+    Console.WriteLine("7 = Raiz de un numero");
+    Console.WriteLine("8 = Seno de un numero");
+    Console.WriteLine("9 = Coseno de un numero");
+    Console.WriteLine("10 = Parte entera de un numero decimal");
     Console.WriteLine("0 = SALIR");
     Console.WriteLine("------------------------------");
     Console.WriteLine("");
@@ -177,6 +183,93 @@ do
             Console.WriteLine("");
             
             break;   
+        case "5":
+            Console.Write("Ingrese un numero: ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese un numero: ");
+                ingreso1 = Console.ReadLine();
+            }
+    
+            Console.WriteLine($"|{num1}| = {Math.Abs(num1)}");
+
+            break;
+        case "6":
+            Console.Write("Ingrese un numero: ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese un numero: ");
+                ingreso1 = Console.ReadLine();
+            }
+    
+            Console.WriteLine($"El cuadrado de {num1} es: {Math.Pow(num1, 2)}");
+
+            break;
+        case "7":
+            Console.Write("Ingrese un numero ");
+            Console.WriteLine("IMPORTANTE: EL NUMERO DEBE SER MAYOR O IGUAL QUE 0");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false || num1 <= 0)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese un numero ");
+            Console.WriteLine("IMPORTANTE: EL NUMERO DEBE SER MAYOR O IGUAL QUE 0");
+                ingreso1 = Console.ReadLine();
+            }
+    
+            Console.WriteLine($"La raíz cuadrada de {num1} es: {Math.Sqrt(num1)}");
+
+            break;
+        case "8":
+            Console.Write("Ingrese un numero: ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese un numero: ");
+                ingreso1 = Console.ReadLine();
+            }
+    
+            Console.WriteLine($"sen({num1}) = {Math.Sin(num1)}");
+
+            break;
+        case "9":
+            Console.Write("Ingrese un numero: ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese un numero: ");
+                ingreso1 = Console.ReadLine();
+            }
+    
+            Console.WriteLine($"cos({num1}) = {Math.Cos(num1)}");
+
+            break;
+        case "10":
+            Console.Write("Ingrese un numero: ");
+            ingreso1 = Console.ReadLine();
+
+            while (double.TryParse(ingreso1, out num1) == false)
+            {
+                Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+                Console.Write("Ingrese un numero: ");
+                ingreso1 = Console.ReadLine();
+            }
+    
+            int Enteronum1 = (int)num1;
+            Console.WriteLine($"La parte entera de {num1} es {Enteronum1}");
+
+            break;
         case "0":
             Console.WriteLine("Saliendo de la calculadora...");
             break;
@@ -188,4 +281,28 @@ do
     }
 
 } while (eleccion != "0"); // no convierto a numero la eleccion ya que solo comparo, lo trabajo como string
-// hacer menu de eleccion de enunciado
+
+// EJERCICIO 3
+Console.WriteLine("MAXIMO ENTRE 2 NUMEROS");
+Console.Write("Ingrese el primer numero a comparar: ");
+ingreso1 = Console.ReadLine();
+
+while (double.TryParse(ingreso1, out num1) == false)
+{
+    Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+    Console.Write("Ingrese el primer numero a comparar: ");
+    ingreso1 = Console.ReadLine();
+}
+
+Console.WriteLine("Ingrese el segundo numero a comparar: ");
+ingreso2 = Console.ReadLine();
+
+while (double.TryParse(ingreso2, out num2) == false || num2 <= 0)
+{
+    Console.WriteLine("Error: Por favor, ingrese un numero válido.");
+    Console.Write("Ingrese el segundo numero a comparar: ");
+    ingreso2 = Console.ReadLine();
+}
+
+Console.WriteLine($"El mayor valor entre {num1} y {num2} es {Math.Max(num1, num2)} y el menor es {Math.Min(num1, num2)}");
+
